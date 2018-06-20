4af00402308810a958416235240c28230e64eb04
wang-song <14384396@qq.com>
2018-06-20 18:50:54 +0800

    第一次提交

---
+++ src/main/java/com/song/blog/dto/MetaDto.java
@@ -0,0 +1,17 @@
+package com.song.blog.dto;
+
+
+import com.song.blog.model.Vo.MetaVo;
+
+public class MetaDto extends MetaVo {
+
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
